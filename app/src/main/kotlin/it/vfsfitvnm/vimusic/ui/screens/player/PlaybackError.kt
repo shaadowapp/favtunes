affcb70a598718168ca7304a64a6363d38dc8708
Daniel <daniel-sevillano@hotmail.com>
2024-11-01 18:39:51 +0100

    Redesigning playback error message

--- app/src/main/kotlin/it/vfsfitvnm/vimusic/ui/screens/player/PlaybackError.kt
+++ app/src/main/kotlin/it/vfsfitvnm/vimusic/ui/screens/player/PlaybackError.kt
@@ -3,15 +3,18 @@ package it.vfsfitvnm.vimusic.ui.screens.player
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Refresh
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,54 +22,52 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import it.vfsfitvnm.vimusic.R
 
 @Composable
 fun PlaybackError(
     isDisplayed: Boolean,
     messageProvider: () -> String,
-    onDismiss: () -> Unit,
-    modifier: Modifier = Modifier
+    onDismiss: () -> Unit
 ) {
-    Box {
-        AnimatedVisibility(
-            visible = isDisplayed,
-            enter = fadeIn(),
-            exit = fadeOut(),
-        ) {
-            Spacer(
-                modifier = modifier
-                    .pointerInput(Unit) {
-                        detectTapGestures(
-                            onTap = {
-                                onDismiss()
-                            }
-                        )
-                    }
-                    .fillMaxSize()
-                    .background(Color.Black.copy(0.8f))
-            )
-        }
-
-        AnimatedVisibility(
-            visible = isDisplayed,
-            enter = slideInVertically { -it },
-            exit = slideOutVertically { -it },
+    AnimatedVisibility(
+        visible = isDisplayed,
+        enter = fadeIn(),
+        exit = fadeOut()
+    ) {
+        Column(
             modifier = Modifier
-                .align(Alignment.TopCenter)
+                .fillMaxSize()
+                .background(Color.Black.copy(0.8F))
+                .padding(all = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(
+                space = 16.dp,
+                alignment = Alignment.CenterVertically
+            ),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
                 text = remember { messageProvider() },
-                style = MaterialTheme.typography.bodyMedium,
+                style = MaterialTheme.typography.bodyLarge,
                 color = Color.White,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .background(Color.Black.copy(0.4f))
-                    .padding(all = 8.dp)
-                    .fillMaxWidth()
+                textAlign = TextAlign.Center
             )
+
+            Button(
+                onClick = onDismiss
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Refresh,
+                    contentDescription = stringResource(id = R.string.retry)
+                )
+
+                Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+
+                Text(text = stringResource(id = R.string.retry))
+            }
         }
     }
 }
